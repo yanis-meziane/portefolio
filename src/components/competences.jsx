9d343ebea4530e926c4932c86607680b6e5bee8c
Yanis Meziane <yanismeziane04@icloud.com>
2025-05-21 18:20:15 +0300

    End of skills part

--- src/components/competences.jsx
+++ src/components/competences.jsx
@@ -6,31 +6,83 @@ export const Competence = () => {
                 <h1>Skills</h1>
 
                 <h2>Front-end</h2>
-                    <article id="skills_main">
+                    <article className="skills_main">
                         <div className="skills">
                             <p>
-                                Hey
+                                Vue.JS
                             </p>
                         </div>
 
                         <div className="skills">
                             <p>
-                                Hey
+                                HTML/CSS
                             </p>
                         </div>
 
                         <div className="skills">
                             <p>
-                                Hey
+                                React
+                            </p>
+                        </div>
+                        <div className="skills">
+                            <p>
+                                JavaScript
+                            </p>
+                        </div>
+
+                        <div className="skills">
+                            <p>
+                                Python
+                            </p>
+                        </div>
+
+                        <div className="skills">
+                            <p>
+                                React
                             </p>
                         </div>
                     </article>
 
                 <h2>Back-end</h2>
-                    <article id="skills_main2">
+                    <article className="skills_main">
                         <div className="skills">
                             <p>
-                                Hey
+                                Java
+                            </p>
+                        </div>
+
+                        <div className="skills">
+                            <p>
+                                Node.js 
+                            </p>
+                        </div>
+                        <div className="skills">
+                            <p>
+                               Python
+                            </p>
+                        </div><div className="skills">
+                            <p>
+                               PHP
+                            </p>
+                        </div>
+                        <div className="skills">
+                            <p>
+                                C
+                            </p>
+                        </div>
+                    </article>
+
+                    <h2>Base De Données</h2>
+                    <article className="skills_main">
+                        <div className="skills">
+                            <p>
+                                PostGreSQL
+                            </p>
+                        </div>
+
+                        <div className="skills">
+                            <p>
+                                MongoDB
                             </p>
                         </div>
                     </article>
