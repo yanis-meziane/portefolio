3bfb733a13a69d4299f29bf42c0598c4bee159bb
Yanis Meziane <yanismeziane04@icloud.com>
2025-05-21 16:42:30 +0300

    Add Skills part

--- src/components/competences.jsx
+++ src/components/competences.jsx
@@ -1,8 +1,39 @@
+import "./CSS/main.css"
+
 export const Competence = () => {
     return(
         <section id="competence">
-            <h1>Je suis la page competence</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, dolorem. Cupiditate quo error eveniet dignissimos, esse et cum, reiciendis dolores, tempore ab quos. Cumque enim blanditiis sed fuga ipsam perferendis.</p>
+                <h1>Skills</h1>
+
+                <h2>Front-end</h2>
+                    <article id="skills_main">
+                        <div className="skills">
+                            <p>
+                                Hey
+                            </p>
+                        </div>
+
+                        <div className="skills">
+                            <p>
+                                Hey
+                            </p>
+                        </div>
+
+                        <div className="skills">
+                            <p>
+                                Hey
+                            </p>
+                        </div>
+                    </article>
+
+                <h2>Back-end</h2>
+                    <article id="skills_main2">
+                        <div className="skills">
+                            <p>
+                                Hey
+                            </p>
+                        </div>
+                    </article>
         </section>
     )
 }
