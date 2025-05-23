2d2952e05df42d293760202b6524ce878528b1ce
Yanis Meziane <yanismeziane04@icloud.com>
2025-05-23 16:48:52 +0300

    Modification

--- src/components/projet.jsx
+++ src/components/projet.jsx
@@ -1,8 +1,78 @@
-export const Projet = () => {
+import "./CSS/main.css";
+import { EyeOff, Framer, MousePointer, Scan} from "lucide-react";
+ 
+ export const Projet = () => {
     return(
-        <section id="projects">
-            <h1>Je suis la page projet</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, assumenda dignissimos? Nesciunt quo facere inventore blanditiis, aspernatur fuga a quisquam voluptas omnis numquam sed doloremque nulla earum officia nostrum voluptatem!</p>
+        <section id="projectsMaster">
+            <div id="projectsMain">
+                <h2>Mes projets</h2>
+                    <article id="projects">
+                        <div className="allProjects">
+                            <div>
+                                <EyeOff />
+                            </div>
+                            <div>
+                                <h3>
+                                    SpeakTouch
+                                </h3>
+                            </div>
+                            <div>
+                                <p>
+                                    Projet visant à aider les personnes rencontrant des difficultés à communiquer verbalement
+                                </p>
+                            </div>
+                        </div>
+                    <br />
+                        <div className="allProjects">
+                            <div>
+                                <Scan />
+                            </div>
+                            <div>
+                                <h3>
+                                    TaskQuests
+                                </h3>
+                            </div>
+                            <div>
+                                <p>
+                                    Projet personnel en cours de réalisation en vue de faciliter la réalisation des tâches d'un projet associatif. Il a pour but de diviser un projet en différents niveau afin de rendre la réalisation de ce projet plus ludique.
+                                </p>
+                            </div>
+                        </div>
+                    <br />
+                        <div className="allProjects">
+                            <div>
+                                <MousePointer />
+                            </div>
+                            <div>
+                                <h3>
+                                    Mini-Jeu en java
+                                </h3>
+                            </div>
+                            <div>
+                                <p>
+                                    Réalisation d'un mini-jeu de combat en Java où nous pouvons choisir ou personnaliser un personnage pour aller combattre des monstres.
+                                </p>
+                            </div>
+                        </div>
+                    <br />
+                        <div className="allProjects">
+                            <div>
+                                <Framer />
+                            </div>
+                            <div>
+                                <h3>
+                                    Script IFS
+                                </h3>
+                            </div>
+                            <div>
+                                <p>
+                                    
+                                </p>
+                            </div>
+                        </div>
+
+                    </article>
+            </div>
         </section>
     )
 }
