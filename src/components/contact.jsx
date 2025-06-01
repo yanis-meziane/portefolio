2e6228083bd945fc29169f63a6c1751e2f70faec
Yanis Meziane <yanismeziane04@icloud.com>
2025-06-01 13:36:45 +0300

    V1 of my portefolio

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,8 +1,64 @@
-export const Contact = () => {
-    return (
-        <section id="contact">
-            <h1>Je suis la partie Contact</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque omnis numquam ducimus molestias sequi aliquid excepturi dolor quia. Sed temporibus sit voluptate illum accusantium! Repellat nesciunt tenetur quaerat blanditiis explicabo!</p>
+import {Headset, Mail} from "lucide-react"
+import "./CSS/main.css";
+
+
+export function Contact() {
+  return (
+    <div>
+            <section id="contactMaster">
+            <div id="contactMain">
+                <h2>Mon contact</h2>
+                    <article id="contact">
+                        <div className="allContact">
+                            <div>
+                                <Headset />
+                            </div>
+                            <div>
+                                <h3>
+                                    Telephone
+                                </h3>
+                            </div>
+                            <div>
+                                <p>
+                                   06.30.38.08.38
+                                </p>
+                            </div>
+                        </div>
+                    <br />
+                        <div className="allContact">
+                            <div>
+                                <Mail />
+                            </div>
+                            <div>
+                                <h3>
+                                    Mails
+                                </h3>
+                            </div>
+                            <div>
+                                <p>
+                                   Personnel : yanismeziane04@icloud.com
+                                </p>
+                            </div>
+                        </div>
+                    <br />
+                        <div className="allContact">
+                            <div>
+                                <linkedin />
+                            </div>
+                            <div>
+                                <h3>
+                                   Linkedin
+                                </h3>
+                            </div>
+                            <div>
+                                <p>
+                                    Yanis Meziane
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+            </div>
         </section>
-    )
+    </div>
+  );
 }
